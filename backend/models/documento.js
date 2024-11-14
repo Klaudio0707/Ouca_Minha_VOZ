cd7929938cb193a73cdaff178102ede9220e1bac
Cláudio Roberto <claudioroberto0707@outlook.com>
2024-11-14 18:13:27 -0300

    Novo início

--- backend/models/documento.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const DocumentoSchema = new mongoose.Schema({
-  empresaId: { type: mongoose.Schema.Types.ObjectId, ref: 'Empresa' },
-  titulo: { type: String, required: true },
-  descricao: { type: String },
-  status: { type: String, default: 'Pendente' },
-  dataEnvio: { type: Date, default: Date.now },
-});
-module.exports = mongoose.model('Documento', DocumentoSchema);
